23b803492469c5ab0452c2404a9eaf448c4561e3
MatiasPineda <matias.pinedav@gmail.com>
2019-12-11 21:18:23 -0300

    bootstrap (25)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
     <Nav />
+    <Contenido />
   </div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
+import Contenido from "./components/Content";
 
 export default {
   name: 'app',
   components: {
+    Contenido,
     Nav
   }
 }
+
 </script>
 
 <style>
+  @import './assets/scss/app.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
