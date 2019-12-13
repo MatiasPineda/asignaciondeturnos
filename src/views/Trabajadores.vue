cee7c81f7cc0856c6261194d1b4c87017d0326af
Daniela Santander <daniela.santander@4agile.cl>
2019-12-13 00:11:39 -0300

    -

--- src/views/Trabajadores.vue
+++ src/views/Trabajadores.vue
@@ -1,10 +1,56 @@
 <template>
-    <h1></h1>
+    <div class="trabajadores">
+        <h1>TRABAJADORES</h1>
+        <div
+            v-for="(trabajador, index) in trabajadores"
+            v-bind:key="index"
+            class="datos-trabajador"
+            style="margin-bottom: 40px"
+        >
+            <p>{{ trabajador.name }}</p>
+            <p>{{ trabajador.email }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Trabajadores"
+        name: "Trabajadores",
+        data() {
+            return {
+                trabajadores: [
+                    {
+                        name: "Juan Pérez",
+                        email: "mail@mail.com",
+                        avaliable: {
+                            lu: "am",
+                            ma: "am",
+                            mi: "pm",
+                            ju: "pm",
+                            vi: "no",
+                            sa: "all",
+                            do: "all"
+                        }
+                    },
+                    {
+                        name: "Matías Pineda",
+                        email: "correo@mail.com",
+                        avaliable: {
+                            lu: "all",
+                            ma: "all",
+                            mi: "no",
+                            ju: "am",
+                            vi: "pm",
+                            sa: "no",
+                            do: "all"
+                        }
+                    },
+                ]
+            }
+        },
+        methods: {
+            listarTrabajadores() {}
+        }
     }
 </script>
 
