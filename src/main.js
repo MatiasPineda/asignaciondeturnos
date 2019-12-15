79da8a451e63364f3bc3379613e9c2cdd22082b7
Daniela Santander <daniela.santander@4agile.cl>
2019-12-15 13:16:35 -0300

    -

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
+import VueMaterial from "vue-material";
 //require("bootstrap");
 
-
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false
 
