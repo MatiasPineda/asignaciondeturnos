286101714f4caabf597280df4d863c22a60dbcf1
Daniela Santander <daniela.santander@4agile.cl>
2019-12-15 21:18:27 -0300

    -

--- src/views/Trabajadores.vue
+++ src/views/Trabajadores.vue
@@ -1,86 +1,64 @@
 <template>
-    <div class="cont-view trabajadores">
-        <h1>TRABAJADORES</h1>
-        <div
-            v-for="(trabajador, index) in trabajadores"
-            v-bind:key="index"
-            class="datos-trabajador"
-            style="margin-bottom: 40px"
-        >
-            <div class="float-left">
-                <img src="../assets/images/chico.svg" class="img-fluid" id="chico">
-                {{ trabajador.name }} -
-                {{ trabajador.email }}
-            </div>
-            <div class="float-right">
-                <md-button @click="editUser(index)" class="md-icon-button md-raised">
-                    <img src="../assets/images/editar.svg" alt="Modificar">
-                </md-button>
-                <md-button @click="showDialog = true" class="md-icon-button md-raised">
-                    <img src="../assets/images/eliminar.svg" alt="Eliminar">
-                </md-button>
-            </div>
-        </div>
-        <md-dialog :md-active.sync="showDialog">
-            <md-dialog-title>¿Está seguro de querer eliminar este usuario?</md-dialog-title>
-            <md-dialog-actions>
-                <md-button class="md-primary" @click="deleteUser(0)">Aceptar</md-button>
-                <md-button class="md-primary" @click="showDialog = false">Cancelar</md-button>
-            </md-dialog-actions>
-        </md-dialog>
+  <div class="cont-view trabajadores">
+    <h1>TRABAJADORES</h1>
+    <div
+      v-for="(trabajador, index) in workers"
+      v-bind:key="index"
+      class="datos-trabajador"
+      style="margin-bottom: 40px"
+    >
+      <div class="float-left">
+        <img src="../assets/images/chico.svg" class="img-fluid" id="chico">
+        {{ trabajador.name }} -
+        {{ trabajador.email }}
+      </div>
+      <div class="float-right">
+        <md-button @click="editUser(index)" class="md-icon-button md-raised">
+          <img src="../assets/images/editar.svg" alt="Modificar">
+        </md-button>
+        <md-button @click="showDialog = true" class="md-icon-button md-raised">
+          <img src="../assets/images/eliminar.svg" alt="Eliminar">
+        </md-button>
+      </div>
     </div>
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>¿Está seguro de querer eliminar este usuario?</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="deleteUser(0)">Aceptar</md-button>
+        <md-button class="md-primary" @click="showDialog = false">Cancelar</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Trabajadores",
-      data() {
-            return {
-              showDialog: false,
-                trabajadores: [
-                    {
-                        name: "Juan Pérez",
-                        email: "mail@mail.com",
-                        available: {
-                            lu: "am",
-                            ma: "am",
-                            mi: "pm",
-                            ju: "pm",
-                            vi: "no",
-                            sa: "all",
-                            do: "all"
-                        }
-                    },
-                    {
-                        name: "Matías Pineda",
-                        email: "correo@mail.com",
-                        available: {
-                            lu: "all",
-                            ma: "all",
-                            mi: "no",
-                            ju: "am",
-                            vi: "pm",
-                            sa: "no",
-                            do: "all"
-                        }
-                    },
-                ]
-            }
-        },
-        methods: {
-          editUser(id){
-            return this.$router.push({
-              name: 'modificar',
-              params: {id: id}
-            })
-          },
-          deleteUser(id){
-            console.log(id);
-            this.showDialog=false;
-          },
-          listarTrabajadores() {}
-        }
+  import { mapGetters } from "vuex";
+
+  export default {
+    name: "Trabajadores",
+    data() {
+      return {
+        showDialog: false,
+        workers: this.$store.state.workers
+      }
+    },
+    computed: {
+      ...mapGetters("workers"),
+    },
+    methods: {
+      editUser(id){
+        return this.$router.push({
+          name: 'modificar',
+          params: {id: id}
+        })
+      },
+      deleteUser(id){
+        console.log(id);
+        this.showDialog=false;
+      },
+      listarTrabajadores() {}
     }
+  }
 </script>
 
 <style scoped>
